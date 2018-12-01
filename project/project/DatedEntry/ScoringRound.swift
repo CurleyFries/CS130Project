6a41d550cef934db4fd34c19d2f2313bbf264f9e
Hajime Hayano <hajimehayano@gmail.com>
2018-11-30 22:13:05 -0800

    [Journal] Implement Journal and DatedEntry
    Changes
    - Implement the class Journal and protocol DatedEntry
    - Implement a JournalBuilder class to help with instantiating the
      Journal class
    
    Unit Tests
    1) JournalTest.swift
    - JournalBuilder class
    - The implementation of the Comparable protocol
    - The Journal class setter methods

---
+++ project/project/DatedEntry/ScoringRound.swift
@@ -0,0 +1,19 @@
+//
+//  ScoringRound.swift
+//  project
+//
+//  Created by Hajime Hayano on 11/28/18.
+//  Copyright © 2018 Chungchhay Kuoch. All rights reserved.
+//
+
+import Foundation
+
+public class ScoringRound: Equatable {
+
+	init() {
+	}
+
+	static public func == (lhs: ScoringRound, rhs: ScoringRound) -> Bool {
+		return true
+	}
+}
